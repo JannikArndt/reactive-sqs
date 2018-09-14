fa8c95f0e8d2e52dfdfca15b355ba02ce79d53f2
Jannik Arndt <jannik@jannikarndt.de>
2018-09-14 17:00:08 +0200

    Added tests

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ libraryDependencies ++= Seq(
   "com.lightbend.akka" %% "akka-stream-alpakka-sqs" % "0.20",
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
   "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+  "org.mockito" % "mockito-core" % "2.22.0",
   "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
   "org.slf4j" % "slf4j-simple" % "1.7.25",
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,16 +1,61 @@
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.testkit.TestKit
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.services.sqs.model.QueueDoesNotExistException
+import com.amazonaws.services.sqs.{AmazonSQSAsync, AmazonSQSAsyncClientBuilder}
+import org.mockito.Mockito._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
-class MainSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpecLike with Matchers with BeforeAndAfterAll {
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
-  def this() = this(ActorSystem("TestSystem"))
+class MainSpec extends TestKit(ActorSystem("TestSystem")) with FlatSpecLike with MockitoSugar with Eventually with Matchers with BeforeAndAfterAll {
+
+  val credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials("x", "x"))
+
+  implicit val awsSqsClient: AmazonSQSAsync = AmazonSQSAsyncClientBuilder
+    .standard()
+    .withCredentials(credentialsProvider)
+    .withEndpointConfiguration(new EndpointConfiguration("http://localhost:4576", "eu-central-1"))
+    .build()
+
+  val queueUrl: String = awsSqsClient.createQueue("integrationtest").getQueueUrl
 
   override def afterAll(): Unit = {
+    awsSqsClient.shutdown()
     shutdown(system)
   }
 
-  "This test" should "do nothing" in {
+  val messageBody = "Example Body"
 
+  // Mock away some business logic
+  val businessLogicMock: BusinessLogic = mock[BusinessLogic]
+  when(businessLogicMock.validateMessage(MyMessage(messageBody))).thenReturn(true)
+
+  "SqsService" should "receive a message" in {
+
+    // Arrange
+    val sqsService = new SqsService(businessLogicMock)
+    sqsService.create(queueUrl, 20)
+
+    // Act: Send message to SQS (synchronous)
+    awsSqsClient.sendMessage(queueUrl, messageBody)
+
+    // Assert: wait for tripService to receive the expected command
+    eventually(timeout(6 seconds)) {
+      verify(businessLogicMock, atLeastOnce()).doBusinessLogic(MyMessage(messageBody))
+      succeed
+    }
+
+  }
+
+  it should "throw an exception if queue doesn't exist" in {
+    val sqsService = new SqsService(businessLogicMock)
+
+    a[QueueDoesNotExistException] should be thrownBy sqsService.create("http://localhost:4576/queue/no_queue", 20)
   }
 }
